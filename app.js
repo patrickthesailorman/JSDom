9bf576fe31a548cf6ec1a126ffea4d51dd581d8d
Patrick McCarty <patrickthesailorman@gmail.com>
2018-12-13 02:14:24 +0000

    Add app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const wmf = document.querySelector('#book-list li:nth-child(2) .name');
 var books = document.querySelector('#book-list li .name');
 // console.log(books);
 
-books = document.querySelectorAll('#wrapper');
+books = document.querySelectorAll('#book-list li .name');
 // console.log(books);
 
 Array.from(books).forEach(function(book){
