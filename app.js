021eed3e3fb8381728b4bf3b7b79596187873760
Patrick McCarty <patrickthesailorman@gmail.com>
2018-12-14 01:43:21 +0000

    Add app.jslesson5

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const books = document.querySelectorAll('#book-list li .name');
 
-Array.from(books).forEach(function(book){
+// Array.from(books).forEach(function(book){
+books.forEach(function(book){
   book.textContent += ' (Book title)';
 });
 
